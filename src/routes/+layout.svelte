b322b8daa646ae8c0f614d298b7e17f331dd92bc
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-05-22 00:45:34 +0900

    💄 레이아웃 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,12 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
+	import './reset.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -31,18 +25,6 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
