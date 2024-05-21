51b87fb1e2a05d7346795aaba7a9917892ddc1f3
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-05-22 00:46:32 +0900

    ✨ How to play 페이지 수정

---
+++ src/routes/how-to-play/+page.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>How to play</title>
+	<meta name="description" content="How to play" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>대충 해 봐 그냥</h1>
+
+	<p>설명설명 <strong>설명</strong> 설명설명설명</p>
+
+	<p>
+		조금 <code>귀찮은데</code> 설명이 필요할까
+	</p>
+
+	<a href="./">돌아가기</a>
+</div>
+
+<style>
+	a {
+		color: white;
+		margin: 20px auto;
+	}
+</style>
